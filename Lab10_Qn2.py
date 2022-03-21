07ce5da7f6f4aa6b6a92138b447efab4dc1006c6
Li-Pin <97491097+Li-Pin@users.noreply.github.com>
2022-03-21 21:43:09 +0800

    Add files via upload

---
+++ Lab10_Qn2.py
@@ -0,0 +1,8 @@
+def average(first, sec):
+    return (first + sec) / 2
+
+
+firstNum = int(input("Please enter the first number: "))
+secNum = int(input("Please enter the second number: "))
+
+print("Average of the 2 numbers: ", average(firstNum, secNum))

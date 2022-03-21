c62434f05e00a2c6944a21cba6688b74886045e8
Li-Pin <97491097+Li-Pin@users.noreply.github.com>
2022-03-21 22:13:58 +0800

    Delete Lab10_Qn2.py

--- Lab10_Qn2.py
+++
@@ -1,8 +0,0 @@
-def average(first, sec):
-    return (first + sec) / 2
-
-
-firstNum = int(input("Please enter the first number: "))
-secNum = int(input("Please enter the second number: "))
-
-print("Average of the 2 numbers: ", average(firstNum, secNum))
